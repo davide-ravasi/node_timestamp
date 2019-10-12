3ce245033ba1c4f006592c43436af87bb3309adf
Glitch (ringed-cough) <none>
2019-10-12 15:23:08 +0000

    👻📫 Checkpoint ./app.js:3838098/221

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var app = express();
 app.get("/", function(req, res) {
   res.send("Hello World!");
 });
+  
+app.get('/api/timestamp/:date_string', function(req, res) {
+  console.log(req.params.date_string);
+})  
 
 app.listen(3000, function() {
   console.log("Example app listening on port 3000!");
