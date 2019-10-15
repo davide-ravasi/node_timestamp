01711ddf2088c1548d8135327ad1d048714871c9
Glitch (ringed-cough) <none>
2019-10-15 14:31:50 +0000

    ✌️🏦 Checkpoint ./app.js:3838098/705

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ var app = express();
 app.get("/", function(req, res) {
   res.send("Hello World!");
 });
+
+app.get('/api/timestamp/', function(req,res) {
+    let newDate = new Date();
+    let newUnixDate = Date.parse(newDate).toString();
+    res.json({"unix": newUnixDate, "utc" : newDate })
+})
   
 app.get('/api/timestamp/:date_string', function(req, res) {
   let dateString = req.params.date_string;
+  console.log(dateString);
   let formattedDate = new Date(dateString);
   let unixDate = Date.parse(formattedDate).toString();
   let response = "";
@@ -14,13 +21,15 @@ app.get('/api/timestamp/:date_string', function(req, res) {
   if(new Date(dateString) == 'Invalid Date') {
     response = "Invalid Date";
     res.json({"error" : "Invalid Date" })
-  } else if(dateString == '') {
-    res.json()
+  } else if(req.param('date_string') === '') {
+    let newDate = new Date();
+    let newUnixDate = Date.parse(newDate).toString();
+    res.json({"unix": newUnixDate, "utc" : newDate })
   } else {  
-    response = unixDate;
+    res.json({"unix": unixDate, "utc" : formattedDate });
   }
   //res.send(formattedDate);
-  res.send(response);
+  //res.send(response);
 })  
 
 app.listen(3000, function() {
