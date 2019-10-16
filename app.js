96f2ec2aa75165b1e8468b2209031446bbe944ed
Glitch (ringed-cough) <none>
2019-10-16 09:07:50 +0000

    🚀🌈 Checkpoint ./app.js:3838098/526

--- app.js
+++ app.js
@@ -18,27 +18,28 @@ app.get('/api/timestamp/:date_string', function(req, res) {
   let response = "";
   console.log("test date url");
   console.log(/^\d*$/.test(req.params.date_string));
-  if(new Date(dateString) == 'Invalid Date') {
-    response = "Invalid Date";
-    res.json({"error" : "Invalid Date" })
-  } else {  
-    if(dateString > 0) {
-      var date = new Date(dateString*1000);
-      // Hours part from the timestamp
-      var hours = date.getHours();
-      // Minutes part from the timestamp
-      var minutes = "0" + date.getMinutes();
-      // Seconds part from the timestamp
-      var seconds = "0" + date.getSeconds();
-      // Will display time in 10:30:23 format
-      var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-      res.json({"unix": dateString, "utc" : formattedTime });
-    } else {
-      res.json({"unix": unixDate, "utc" : formattedDate });
-    }
+  res.json({"unix": unixDate, "utc" : formattedDate, "unixdate": "false" });
+  // if(new Date(dateString) == 'Invalid Date') {
+  //   response = "Invalid Date";
+  //   res.json({"error" : "Invalid Date" })
+  // } else {  
+//     if(dateString.indexOf("-")) {
+//       var date = new Date(dateString*1000);
+//       // Hours part from the timestamp
+//       var hours = date.getHours();
+//       // Minutes part from the timestamp
+//       var minutes = "0" + date.getMinutes();
+//       // Seconds part from the timestamp
+//       var seconds = "0" + date.getSeconds();
+//       // Will display time in 10:30:23 format
+//       var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+//       res.json({"unix": dateString, "utc" : formattedTime, "unixdate": "true" });
+//     } else {
+//       res.json({"unix": unixDate, "utc" : formattedDate, "unixdate": "false" });
+//     }
     
     
-  }
+  // }
 })  
 
 app.listen(3000, function() {
