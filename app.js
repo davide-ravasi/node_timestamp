01017ba6358a7e0bee89d9354169ccc411dd5635
Glitch (ringed-cough) <none>
2019-10-16 10:56:29 +0000

    🏀🐓 Checkpoint ./app.js:3838098/103

--- app.js
+++ app.js
@@ -15,21 +15,23 @@ app.get('/api/timestamp/:date_string', function(req, res) {
     res.json({"error" : "Invalid Date" })
   } else {  
     if(dateString.indexOf("-") == -1) {
-      var a = new Date(dateString * 1000);
-      var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-      var year = a.getFullYear();
+      let a = new Date(dateString * 1000);
+      let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+      let year = a.getFullYear();
       var month = months[a.getMonth()];
-      var date = a.getDate();
-      var hour = a.getHours();
-      var min = a.getMinutes();
-      var sec = a.getSeconds();
-      var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+      let date = a.getDate();
+      let hour = a.getHours();
+      let min = a.getMinutes();
+      let sec = a.getSeconds();
+      let time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
       res.json({"unix": dateString, "utc" : time});
     } else {
       let formattedDate = new Date(dateString);
       let unixDate = Date.parse(formattedDate);
       res.json({"unix": unixDate, "utc" : formattedDate});
     }
+    
+    res.json({"unix": dateString, "utc" : time});
   }
 })  
 
