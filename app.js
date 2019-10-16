370a0ce49070f334feaba4b0a4fe27579d81cdb8
Glitch (ringed-cough) <none>
2019-10-16 08:55:51 +0000

    🏫🐒 Checkpoint ./app.js:3838098/1081

--- app.js
+++ app.js
@@ -22,7 +22,22 @@ app.get('/api/timestamp/:date_string', function(req, res) {
     response = "Invalid Date";
     res.json({"error" : "Invalid Date" })
   } else {  
-    res.json({"unix": unixDate, "utc" : formattedDate });
+    if(dateString > 0) {
+      var date = new Date(dateString*1000);
+      // Hours part from the timestamp
+      var hours = date.getHours();
+      // Minutes part from the timestamp
+      var minutes = "0" + date.getMinutes();
+      // Seconds part from the timestamp
+      var seconds = "0" + date.getSeconds();
+      // Will display time in 10:30:23 format
+      var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+      res.json({"unix": dateString, "utc" : formattedTime });
+    } else {
+      res.json({"unix": unixDate, "utc" : formattedDate });
+    }
+    
+    
   }
 })  
 
